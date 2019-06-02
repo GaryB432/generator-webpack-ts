eb57b94f474be195a3a13f7a57b8252f7f4863f6
Gary Bortosky <garybort@gmail.com>
2019-06-01 22:06:40 -0500

    fix sw

--- generators/app/templates/src/sw.js
+++ generators/app/templates/src/sw.js
@@ -1,12 +1,3 @@
-function addIndexRevision(urls, defRev) {
-  const rando = (Math.random() * 1000000).toPrecision(6);
-  const indices = __precacheManifest.filter(m => m.url === '/index.html');
-  const revision = indices.length === 1 ? indices[0].revision : defRev || rando;
-  return urls.map(url => {
-    return { url, revision };
-  });
-}
-
 workbox.core.setCacheNameDetails({
   prefix: '<%= appname %>',
   suffix: 'v1',
@@ -15,14 +6,11 @@ workbox.core.setCacheNameDetails({
   googleAnalytics: 'ga',
 });
 
-workbox.precaching.precache(['/img/yeoman-033.png']);
+workbox.precaching.precache(['/img/yeoman-003.png']);
 
-workbox.precaching.precache(addIndexRevision(['/', '/manifest.json']));
+workbox.core.skipWaiting();
+workbox.core.clientsClaim();
 
-workbox.skipWaiting();
-workbox.clientsClaim();
-
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {
   cleanUrls: false,
 });
